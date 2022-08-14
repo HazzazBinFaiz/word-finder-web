447bd9dd1c89c16013f3b4a6872a1c431b9d547b
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-08-15 01:07:01 +0600

    Extract result panel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Header from './Header'
 import ResultPanel from './ResultPanel'
 import SearchInput from './SearchInput'
-import words from './words.json'
 
 function App() {
 
   const [term, setTerm] = useState('')
-  const [result, setResult] = useState<string[]>([])
-
-  useEffect(() => {
-    if (term === '') {
-      setResult([])
-      return
-    }
-
-    let searchTerm = term
-      .replaceAll('-', '([a-z]*)')
-    let pattern = new RegExp('^'+searchTerm)
-    setResult(
-      words.map(word => word.match(pattern))
-        .filter((match : RegExpMatchArray | null) : match is RegExpMatchArray  => {
-          return match !== null && match !== undefined;
-        })
-        .filter(match => {
-          return match.input !== undefined && match.input.length
-        })
-        .slice(0, 20)
-        .sort((a, b) => {
-          return b.join().length - a.join().length
-        })
-        .map((match) => {
-          let original = match.input ?? ''
-          let matched = match[0]
-          for (let i = 1; i < match.length; i++) {
-            matched = matched.replace(match[i], '<span class="text-green-600">' + match[i] + '</span>')
-          }
-          return original.replace(match[0], '<span class="text-black font-semibold">' + matched + '</span>')
-        })
-    )
-  }, [term])
 
   return (
     <div className='h-screen flex flex-col'>
       <Header className='w-full bg-slate-700' />
-      <ResultPanel className='w-full max-w-5xl mx-auto flex-grow p-4 overflow-auto' result={result} />
+      <ResultPanel className='w-full max-w-5xl mx-auto flex-grow p-4 overflow-auto' term={term} />
       <SearchInput className='w-full max-w-5xl mx-auto p-4' term={term} setTerm={setTerm}/>
     </div>
   )
--- src/ResultPanel.tsx
+++ src/ResultPanel.tsx
@@ -1,14 +1,59 @@
+import { useState, useEffect } from "react";
 import { HasClassName } from "./types";
+import words from './words.json'
 
 type ResultPanelProps = {
-    result: string[]
+    term: string
 } & HasClassName
 
 
+const rejectNullMatch = (match: RegExpMatchArray | null) : match is RegExpMatchArray => {
+    return match !== null;
+}
+
+type RegExpMatchArrayWithInput = RegExpMatchArray & {
+    input: string
+}
+
+const rejectBlankMatch = (match: RegExpMatchArray) : match is RegExpMatchArrayWithInput => {
+    return (match.input ?? '').length > 0
+}
+
 function ResultPanel(props: ResultPanelProps) {
+
+    const [result, setResult] = useState<string[]>([])
+
+    useEffect(() => {
+        if (props.term === '') {
+            setResult([])
+            return
+        }
+
+        let searchTerm = props.term
+            .replaceAll('-', '([a-z]*)')
+        let pattern = new RegExp('^' + searchTerm)
+        setResult(
+            words.map(word => word.match(pattern))
+                .filter(rejectNullMatch)
+                .filter(rejectBlankMatch)
+                .slice(0, 50)
+                .sort((a, b) => {
+                    return (a.input.length - a[0].length) - (b.input.length - b[0].length)
+                })
+                .map((match) => {
+                    let original = match.input ?? ''
+                    let matched = match[0]
+                    for (let i = 1; i < match.length; i++) {
+                        matched = matched.replace(match[i], '<span class="text-green-600">' + match[i] + '</span>')
+                    }
+                    return original.replace(match[0], '<span class="text-black font-semibold">' + matched + '</span>')
+                })
+        )
+    }, [props.term])
+
     return (
         <div className={props.className}>
-            {props.result.map(item => (
+            {result.map(item => (
                 <div className="w-full p-2 text-lg text-slate-600 text-center border-t border-x last:border-b" dangerouslySetInnerHTML={{ __html: item }}></div>
             ))}
         </div>
