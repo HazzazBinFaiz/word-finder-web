ebbfe9cc7407662af35c039aebf1e193931b1774
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-08-14 20:21:06 +0600

    Add pathReplecer plugin to support sub path instead of main domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const pathReplecer = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html) {
+      return html.replace(
+        /(href|src)="\//g,
+        '$1="'
+      )
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), pathReplecer()]
 })
