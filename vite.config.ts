211a3b89717915ae58537d9620ee1274e49d3926
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-08-20 21:04:57 +0600

    Make application PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const pathReplecer = () => {
   return {
@@ -15,5 +16,38 @@ const pathReplecer = () => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pathReplecer()]
+  plugins: [
+    react(),
+    pathReplecer(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon-192x192.png', 'icon-256x256.png', 'icon-384x384.png', 'icon-512x512.png'],
+      manifest: {
+        name: 'Word Finder',
+        short_name: 'word-finder',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-256x256.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-384x384.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
