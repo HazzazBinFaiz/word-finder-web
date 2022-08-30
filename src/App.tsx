98d6288473ab3a7a67a1ff2d1b2756d9c323dce8
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-08-30 23:07:11 +0600

    Allow user to search from url query string

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,25 @@ function App() {
 
   useEffect(() => {
     document.getElementById('loading')?.remove()
-    
-    setTerm('-')
-    setTimeout(() => {
-      setTerm('')
-    }, 1)
-   
+
+    const queryTerm = (new URLSearchParams(location.search).get('term') ?? '')
+      .toLowerCase().replace(/[^a-z\-]/g, '');
+    const dataLoaded = localStorage.getItem('data-loaded')
+
+    if (term.length === 0 && queryTerm.length > 0) {
+      setTerm(queryTerm)
+    } else if (!dataLoaded) {
+      setTerm('-')
+      setTimeout(() => {
+        setTerm('')
+      }, 1)
+      localStorage.setItem('data-loaded', 'true')
+    }
   }, []);
 
   return (
     <div className='h-screen flex flex-col'>
-      <Header className='w-full bg-slate-700' term={term} setTerm={setTerm}/>
+      <Header className='w-full bg-slate-700' term={term} setTerm={setTerm} />
       <ResultPanel className='w-full max-w-5xl mx-auto flex-grow overflow-auto' term={term} />
     </div>
   )
